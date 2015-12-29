a68e0bfd8d9558222d085300cffc67aa9935957e
Sam Hissaund <cplsyx@gmail.com>
2015-12-29 00:20:49 +0000

    Added threading, default 500ms interval

--- ProcessScreengrab/ProcessScreengrab/Form1.Designer.cs
+++ ProcessScreengrab/ProcessScreengrab/Form1.Designer.cs
@@ -34,6 +34,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
@@ -41,9 +42,9 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(440, 220);
+            this.button1.Location = new System.Drawing.Point(446, 220);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(178, 58);
+            this.button1.Size = new System.Drawing.Size(77, 58);
             this.button1.TabIndex = 0;
             this.button1.Text = "Process";
             this.button1.UseVisualStyleBackColor = true;
@@ -82,7 +83,7 @@
             // 
             this.numericUpDown1.Location = new System.Drawing.Point(370, 258);
             this.numericUpDown1.Maximum = new decimal(new int[] {
-            30,
+            1000,
             0,
             0,
             0});
@@ -97,7 +98,7 @@
             this.numericUpDown1.TabStop = false;
             this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.numericUpDown1.Value = new decimal(new int[] {
-            5,
+            500,
             0,
             0,
             0});
@@ -107,9 +108,20 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(367, 243);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 5;
-            this.label1.Text = "Interval";
+            this.label1.Text = "Interval (ms)";
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(529, 220);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 58);
+            this.button2.TabIndex = 0;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
@@ -121,9 +133,11 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Process Screengrab";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
@@ -140,6 +154,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- ProcessScreengrab/ProcessScreengrab/Form1.cs
+++ ProcessScreengrab/ProcessScreengrab/Form1.cs
@@ -7,47 +7,69 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace ProcessScreengrab
 {
     public partial class Form1 : Form
     {
+        bool processing = false;
+
         public Form1()
         {
             InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            using (Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
-                                            Screen.PrimaryScreen.Bounds.Height))
+            processing = true;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            new Thread(CaptureScreen).Start();
+
+        }
+
+        private void CaptureScreen()
+        {
+            while (processing)
             {
-                using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+                using (Bitmap bmpScreenCapture = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height))
                 {
-                    g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
-                                     Screen.PrimaryScreen.Bounds.Y,
-                                     0, 0,
-                                     bmpScreenCapture.Size,
-                                     CopyPixelOperation.SourceCopy);
+                    using (Graphics g = Graphics.FromImage(bmpScreenCapture))
+                    {
+                        g.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                         Screen.PrimaryScreen.Bounds.Y,
+                                         0, 0,
+                                         bmpScreenCapture.Size,
+                                         CopyPixelOperation.SourceCopy);
+                    }
+
+                    //Comment out the below line for pictureBox1 to improve performance
+                    //pictureBox1.Image = new Bitmap(bmpScreenCapture, new Size(pictureBox1.Width, pictureBox1.Height));
+
+
+                    pictureBox2.BackColor = ProcessImage(bmpScreenCapture);
+
+                    setDetail("Bitmap Size:" + bmpScreenCapture.Width + " x " + bmpScreenCapture.Height + "\r\nColour value of screen:" + pictureBox2.BackColor.ToString());
+
                 }
-
-                pictureBox1.Image = new Bitmap(bmpScreenCapture, new Size(pictureBox1.Width, pictureBox1.Height));
-                pictureBox2.BackColor = ProcessImage(bmpScreenCapture);
-
+                Thread.Sleep((int)numericUpDown1.Value);
             }
 
         }
 
-        private Color ProcessImage(Bitmap b)
+        private Color ProcessImage(Bitmap bm)
         {
+            FastBitmap fb = new FastBitmap(bm);
+            fb.Lock();
             int total = 0;
             long[] colors = new long[] { 0, 0, 0 };
-            int skip = b.Width * b.Height / 50000;
-            for (int x = 0; x < b.Width; x += skip)
+            for (int x = 0; x < fb.Width; x += 50)
             {
-                for (int y = 0; y < b.Height; y += skip)
+                for (int y = 0; y < fb.Height; y += 50)
                 {
-                    Color pixel = b.GetPixel(x, y);
+                    Color pixel = fb.GetPixel(x, y);
                     colors[0] += pixel.R;
                     colors[1] += pixel.G;
                     colors[2] += pixel.B;
@@ -59,8 +81,35 @@ namespace ProcessScreengrab
             green = (byte)((double)colors[1] / total);
             blue = (byte)((double)colors[2] / total);
             Color col = Color.FromArgb(red, green, blue);
+            fb.Unlock();
             return col;
 
         }
+
+        private void setDetail(String s)
+        {
+            try
+            {
+                textBox1.Text = s;
+            } 
+            catch (Exception)
+            {
+                //Do nothing as this will be a result of the form closing whilst trying to update the text box control.
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            processing = false;
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            processing = false;
+        }
+
     }
+
 }
